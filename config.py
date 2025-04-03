a5fffb88979e3ee572066c9989de1ec104f8bd5e
Lyfspongebob <3080291974@qq.com>
2025-04-03 08:14:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,15 @@ class Config:
     # 会话配置
     SESSION_TYPE = 'sqlalchemy'
     SESSION_SQLALCHEMY_TABLE = 'sessions'  # 自定义会话表名
-    PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=10)
 
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
         "pool_recycle": 3600,
         "max_overflow": 10
     }
+
+    #你使用的大模型的api_url/api_key/knowledge_base_id
+    MODEL_API_URL = 'YOUR_MODEL_API_URL'
+    MODEL_API_KEY = 'YOUR_MODEL_API_KEY'
+    KNOWLEDGE_BASE_ID = 'YOUR_KNOWLEDGE_BASE_ID'
